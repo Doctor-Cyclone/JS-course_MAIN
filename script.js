d867b7c83ba7c29758282d0807269e86fffc99ee
Kirill Demidov <kirill.demidov.1995@mail.ru>
2021-02-01 22:56:28 +0300

    Изменил Number() на +

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 'use strict';
 
-const money = Number(prompt('Ваш месячный доход?')),
-income = 'фриланс',
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
-deposit = confirm('Есть ли у вас депозит в банке?');
-
-const expenses1 = prompt('Введите обязательную статью расходов'),
-amount1 = Number(prompt('Во сколько это обойдется?')),
-expenses2 = prompt('Введите обязательную статью расходов'),
-amount2 = Number(prompt('Во сколько это обойдется?')),
-mission = 1000000,
-period = 6,
-lowerCase = addExpenses.toLowerCase(),
-budgetMonth = money - (amount1 + amount2),
-budgetDay = Math.floor(budgetMonth / 30),
-missionTime = Math.ceil(mission / budgetMonth);
+const money = +prompt('Ваш месячный доход?'),
+    income = 'фриланс',
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
+    expenses1 = prompt('Введите обязательную статью расходов'),
+    amount1 = +prompt('Во сколько это обойдется?'),
+    expenses2 = prompt('Введите обязательную статью расходов'),
+    amount2 = +prompt('Во сколько это обойдется?'),
+    mission = 1000000,
+    period = 6,
+    lowerCase = addExpenses.toLowerCase(),
+    budgetMonth = money - (amount1 + amount2),
+    budgetDay = Math.floor(budgetMonth / 30),
+    missionTime = Math.ceil(mission / budgetMonth);
 
 console.log(typeof money);
 console.log(typeof income);
@@ -33,7 +32,6 @@ console.log('Бюджет на месяц = ' + budgetMonth);
 console.log('Бюджет на день = ' + budgetDay);
 
 console.log(mission + ' вы накопите за ' + missionTime);
-
 
 if (budgetDay >= 1200) {
     console.log('У вас высокий уровень дохода');
