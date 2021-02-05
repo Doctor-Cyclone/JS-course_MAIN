bdde6f307e86c5d8aca41f9814ac19a5cd2f47ce
Kirill Demidov <kirill.demidov.1995@mail.ru>
2021-02-05 19:31:56 +0300

    Исправлен объект expence

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ appData.getBudget();
 console.log('Расходы за месяц: ' + appData.expensesMonth);
 console.log(appData.getTargetMonth());
 console.log(appData.getStatusIncome(appData.budgetDay));
-console.log(appData.expenses);
+
 for(let key in appData){
     console.log('Наша программа включает в себя данные: ' + key + ' ' + appData[key]);
 }
